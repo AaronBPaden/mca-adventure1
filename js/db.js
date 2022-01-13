71ec7ada03611ae11f8aaf83bc1934deec25a5f4
Aaron Paden <aaronbpaden@gmail.com>
2022-01-13 14:20:40 -0600

    Also convert entities to an object.

--- js/db.js
+++ js/db.js
@@ -6,10 +6,10 @@ import { state } from './state.js';
 
 export let db = {
     rooms: {
-        westOfHouse: new Room(0, "West of House", "This is west of house.", {north: "northOfHouse", east: null, west: null, south: null}, false, '../media/backgrounds/west-of-house.webp', [0]),
+        westOfHouse: new Room(0, "West of House", "This is west of house.", {north: "northOfHouse", east: null, west: null, south: null}, false, '../media/backgrounds/west-of-house.webp', ["mailbox"]),
         northOfHouse: new Room(1, "North of House", "This is north of house.", {north: null, east: null, west: null, south: "westOfHouse"}, true, '../media/backgrounds/north-of-house.webp'),
     },
-    entities: [
-        new Entity(0, "mailbox", "it's a mailbox", ["mailbox.png"], 200, 400, 100, 100, 0, ["mailbox0"]),
-    ],
+    entities: {
+        mailbox: new Entity(0, "mailbox", "it's a mailbox", ["mailbox.png"], 200, 400, 100, 100, 0, ["mailbox0"]),
+    },
 }
